432f33637516374a916ce3a8929e723c596f1f1e
Sebastian Qu <sebastianqu@gmail.com>
2016-02-02 17:56:22 +0800

    webpack

---
+++ web-root/app/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.createElement('div');
+document.body.appendChild(app);
+
+ReactDOM.render(
+    <h1>Hello, world!!<div> </h1>,
+    app
+);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var WEB_ROOT_PATH = path.resolve(ROOT_PATH, 'web-root');
+var APP_PATH = path.resolve(WEB_ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(WEB_ROOT_PATH, 'build');
+
+module.exports = {
+    entry: {
+        app: path.resolve(APP_PATH, 'index.jsx')
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    //enable dev source map
+    devtool: 'eval-source-map',
+    //enable dev server
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    //babel重要的loader在这里
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['babel'],
+            include: APP_PATH,
+        }]
+    },
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: 'My first react app'
+        })
+    ]
+};
